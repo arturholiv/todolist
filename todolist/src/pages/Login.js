8ddf97acc6f27177dec3e2cd64b27dcf7aa4cc88
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-03-21 17:35:34 -0300

    feat: add login page

---
+++ todolist/src/pages/Login.js
@@ -0,0 +1,88 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import loginEmail from '../actions';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emailInputText: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  validateEmail(email) {
+    const regex = /\S+@\S+\.\S+/;
+    return regex.test(email);
+  }
+
+  render() {
+    const { password, emailInputText } = this.state;
+    const { dispatchUserEmail } = this.props;
+    const minPasswordLength = 6;
+    return (
+      <div>
+        <form className="form__group field">
+          <label
+            htmlFor="email-input"
+            className="form__label"
+          >
+            <input
+              type="email"
+              name="emailInputText"
+              placeholder="Email"
+              data-testid="email-input"
+              required
+              className="form__field"
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <label
+            htmlFor="password-input"
+            className="form__label"
+          >
+            <input
+              type="password"
+              name="password"
+              minLength="6"
+              placeholder="Senha"
+              data-testid="password-input"
+              required
+              className="form__field"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <Link to="/carteira">
+            <button
+              type="submit"
+              disabled={ !(this.validateEmail(emailInputText)
+              && password.length >= minPasswordLength) }
+              onClick={ () => dispatchUserEmail(emailInputText) }
+            >
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </div>);
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchUserEmail: (email) => dispatch(loginEmail(email)),
+});
+
+Login.propTypes = {
+  dispatchUserEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
