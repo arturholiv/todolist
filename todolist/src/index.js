a6eaa8c1b7f5d8ac17407345e5d016df6c665a81
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-03-22 17:23:11 -0300

    feat: changes route

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// o provider é o meio pelo qual disponibilizamos o store
-// com os estados de toda a aplicação para todos os demais componentes
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
@@ -16,7 +14,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
